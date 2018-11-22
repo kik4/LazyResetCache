96730e762d0a156eadf3359510b079b4574a3fab
kik4 <kik4.contact@gmail.com>
2018-11-22 16:27:42 +0900

    Initial commit

---
+++ CacheService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace CacheService.Tests
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Fact]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+    }
+}
