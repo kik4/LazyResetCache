78a1e3aac97ecbfe91bcc811209bb40205e5e2b3
kik4 <kik4.contact@gmail.com>
2018-11-23 15:42:09 +0900

    impl sample

--- LazyResetCache.Sample/Controllers/ValuesController.cs
+++ LazyResetCache.Sample/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LazyResetCache;
 
 namespace LazyResetCache.Sample.Controllers
 {
@@ -10,36 +11,20 @@ namespace LazyResetCache.Sample.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        static LazyResetCache<string> cache = new LazyResetCache<string>(new TimeSpan(0, 0, 3));
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<string> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
+            var key = "hoge";
+            var cached = cache.Exists(key);
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            if (!cached)
+            {
+                cache.Set(key, () => DateTime.Now.ToString());
+            }
+            return (cached ? "from " : "new ") + "lazy cache: " + cache.Get(key);
         }
     }
 }
